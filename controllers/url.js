9ddd4d5550eb8cdc18c4da6be67c304ba00b9352
M Salman Khan <thesalmankhan2021@gmail.com>
2025-06-02 20:41:57 +0530

    Introduce View_Templete for Frontend Supports

--- controllers/url.js
+++ controllers/url.js
@@ -4,17 +4,16 @@ const URL = require('../models/url')
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
     if (!body.url) return res.status(400).json({ error: "url is required" });
-    const entry = await URL.findOne({redirectURL: `https://${body.url}`},"shortId")
-    console.log(entry)
+    const entry = await URL.findOne({redirectURL: body.url},"shortId")
     if(entry)  return res.json({ id: entry.shortId });
     const shortID = nanoid(8);
     await URL.create({
         shortId: shortID,
-        redirectURL:`https://${body.url}`,
+        redirectURL:body.url,
         visitHistory:[]
 
     })
-    return res.json({id:shortID})
+    return res.render("home",{id:shortID})
 }
 
 module.exports={
--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const { connectToMongoDB } = require('./connect')
 const PORT = 8000
 const URL = require("./models/url")
 const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+    .then(() => console.log("Connected to MongoDB Successfully"))
+
+
+app.set("view engine", "ejs")
+
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.use('/url', urlRoute)
-// app.get('/test',async(req,res)=>{
-//     const allUrls = await URL.find({})
-//     console.log(allUrls)
-    
-//     return res.end(`<html>
-//         <head>
-//         <title>Heading</title>
-//         </head>
-//         <body>
-//         <ol>
-//             ${allUrls.map(url => (
-//                 `<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}</li>`
-//             )).join('')}
-//         </ol>
-//         </body>
-//         </html>`);
-// })
+app.use('/', staticRoute)
+
+
 
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId
@@ -37,10 +35,8 @@ app.get('/url/:shortId', async (req, res) => {
                 }
             }
         })
-        res.redirect(entry.redirectURL)
+    res.redirect(entry.redirectURL)
 })
 
 
-connectToMongoDB("mongodb://localhost:27017/short-url")
-    .then(() => console.log("Connected to MongoDB Successfully"))
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
